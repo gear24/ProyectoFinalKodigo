74c8d2de0b34c0a55466b201a908d776d84a0db5
Gustavo <gear7082@gmail.com>
2024-10-24 20:09:03 -0600

    Loading animations for Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,33 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth } from './Peticiones/Request';
 import { useNavigate } from 'react-router-dom';
-
+import { MutatingDots } from 'react-loader-spinner'; // Importa el spinner
 
 const Dashboard = () => {
   const { getAllBootcamps, bootcamps, deactivateBootcamp, loginUser } = useAuth();
   const navigate = useNavigate();
-  const [token, setToken] = useState(null); // Almacenar el token
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true); // Estado de carga
+  const [error, setError] = useState(null); // Estado para manejar mensajes de error
 
-
-
-  // Credenciales quemadas 
   const username = 'cheetos';
   const password = '123456789';
 
   useEffect(() => {
     const loginAndFetchBootcamps = async () => {
       try {
-        // Login automático
         const response = await loginUser(username, password);
         const token = response.token; 
         setToken(token);
 
-        // con el token pa'cer la solicitud para obtener los bootcamps
         if (token) {
           await getAllBootcamps(token);
         }
       } catch (error) {
         console.error('Error al iniciar sesión y obtener bootcamps:', error);
+        setError('No se pudo iniciar sesión. Por favor, verifica tus credenciales.'); // Establece el mensaje de error
+      } finally {
+        setLoading(false); // Cambia el estado de carga a false al final
       }
     };
 
@@ -36,12 +36,20 @@ const Dashboard = () => {
 
   const handleDeleteBootcamp = async (id) => {
     if (window.confirm('¿Estás seguro de que deseas eliminar este bootcamp?')) {
-      await deactivateBootcamp(token, id); // se usa el token almacenado
+      await deactivateBootcamp(token, id);
     }
   };
 
-  if (!token) {
-    return <h2>Cargando... Iniciando sesión...</h2>; // Puedes manejar un estado de carga
+  if (loading) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <MutatingDots color="#00BFFF" height={100} width={100} />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <h2>{error}</h2>; // Muestra el mensaje de error
   }
 
   return (
